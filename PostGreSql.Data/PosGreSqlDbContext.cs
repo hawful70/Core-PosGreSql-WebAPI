cdb3977a80812b5b952ea1c75ce77b5bd14ece3e
Hau le <hault6@fsoft.com.vn>
2018-09-03 21:57:45 +0700

    Implement System.Data.Entity and Add DbContext

---
+++ PostGreSql.Data/PosGreSqlDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Data.Entity;
+
+namespace PostGreSql.Data
+{
+    public class PosGreSqlDbContext : DbContext
+    {
+        public PosGreSqlDbContext() : base("PosGreSqlDbContext")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
