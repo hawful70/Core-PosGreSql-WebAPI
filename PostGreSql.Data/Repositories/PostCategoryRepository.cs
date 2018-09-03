d06e549bf56881b6db48f92db90755a1723d06e8
Hau le <hault6@fsoft.com.vn>
2018-09-03 23:48:05 +0700

    implement Repositories

---
+++ PostGreSql.Data/Repositories/PostCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using CorePosGreSqlWebAPI.Data.Infrastructure;
+using PostGreSql.Model;
+
+namespace CorePosGreSqlWebAPI.Data.Repositories
+{
+    public interface IPostCategoryRepository : IRepository<PostCategory>
+    {
+    }
+
+    public class PostCategoryRepository : RepositoryBase<PostCategory>, IPostCategoryRepository
+    {
+        public PostCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ PostGreSql.Data/Repositories/TagRepository.cs
@@ -0,0 +1,16 @@
+﻿using CorePosGreSqlWebAPI.Data.Infrastructure;
+using PostGreSql.Model;
+
+namespace CorePosGreSqlWebAPI.Data.Repositories
+{
+    public interface ITagRepository : IRepository<Tag>
+    {
+    }
+
+    public class TagRepository : RepositoryBase<Tag>, ITagRepository
+    {
+        public TagRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
