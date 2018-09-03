f3c2d44f7bbe4bf96df16cb31ec0003c88e20723
Hau le <hault6@fsoft.com.vn>
2018-09-03 22:23:17 +0700

    implement migration

---
+++ PostGreSql.Data/Migrations/201809031521491_init data.cs
@@ -0,0 +1,97 @@
+namespace PostGreSql.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initdata : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PostCategories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Alias = c.String(nullable: false, maxLength: 256),
+                        Description = c.String(maxLength: 500),
+                        ParentID = c.Int(),
+                        DisplayOrder = c.Int(),
+                        Image = c.String(maxLength: 256),
+                        HomeFlag = c.Boolean(),
+                        CreatedDate = c.DateTime(),
+                        CreatedBy = c.String(maxLength: 256),
+                        UpdatedDate = c.DateTime(),
+                        UpdatedBy = c.String(maxLength: 256),
+                        MetaKeyword = c.String(maxLength: 256),
+                        MetaDescription = c.String(maxLength: 256),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Alias = c.String(nullable: false, maxLength: 256),
+                        CategoryID = c.Int(nullable: false),
+                        Image = c.String(maxLength: 256),
+                        Description = c.String(maxLength: 500),
+                        Content = c.String(),
+                        HomeFlag = c.Boolean(),
+                        HotFlag = c.Boolean(),
+                        ViewCount = c.Int(),
+                        CreatedDate = c.DateTime(),
+                        CreatedBy = c.String(maxLength: 256),
+                        UpdatedDate = c.DateTime(),
+                        UpdatedBy = c.String(maxLength: 256),
+                        MetaKeyword = c.String(maxLength: 256),
+                        MetaDescription = c.String(maxLength: 256),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.PostCategories", t => t.CategoryID, cascadeDelete: true)
+                .Index(t => t.CategoryID);
+            
+            CreateTable(
+                "dbo.PostTags",
+                c => new
+                    {
+                        PostID = c.Int(nullable: false),
+                        TagID = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => new { t.PostID, t.TagID })
+                .ForeignKey("dbo.Posts", t => t.PostID, cascadeDelete: true)
+                .ForeignKey("dbo.Tags", t => t.TagID, cascadeDelete: true)
+                .Index(t => t.PostID)
+                .Index(t => t.TagID);
+            
+            CreateTable(
+                "dbo.Tags",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 50),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Type = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.PostTags", "TagID", "dbo.Tags");
+            DropForeignKey("dbo.PostTags", "PostID", "dbo.Posts");
+            DropForeignKey("dbo.Posts", "CategoryID", "dbo.PostCategories");
+            DropIndex("dbo.PostTags", new[] { "TagID" });
+            DropIndex("dbo.PostTags", new[] { "PostID" });
+            DropIndex("dbo.Posts", new[] { "CategoryID" });
+            DropTable("dbo.Tags");
+            DropTable("dbo.PostTags");
+            DropTable("dbo.Posts");
+            DropTable("dbo.PostCategories");
+        }
+    }
+}
